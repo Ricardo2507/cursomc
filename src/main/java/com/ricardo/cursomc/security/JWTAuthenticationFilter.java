ecbd3c89f89f5bc2b8d87a7e5d82c29cf7337b46
Ricardo Sérgio <rscarvalho@hotmail.com>
2020-04-01 22:16:57 -0300

    Revert "Expondo o header Authorization"
    This reverts commit 26330664641b144ef046d646611df0f46dcf629b.

--- src/main/java/com/ricardo/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/ricardo/cursomc/security/JWTAuthenticationFilter.java
@@ -58,8 +58,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
-        res.addHeader("Authorization", "Bearer " + token); // não é aceito pelo mecanismo de cors
-        res.addHeader("access-control-expose-headers", "Authorization");
+        res.addHeader("Authorization", "Bearer " + token);
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
